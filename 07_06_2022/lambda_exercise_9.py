8ca919e52f14e5dabbe5359a3384f51e2346f72c
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-07 15:14:11 +0530

    lambda ex 9

---
+++ 07_06_2022/lambda_exercise_9.py
@@ -0,0 +1,12 @@
+# 9) Write a Python program to find the elements of a given list of strings that contain specific substring using
+# lambda.
+# Original list: ['red', 'black', 'white', 'green', 'orange']
+# Substring to search: ack
+# Elements of the said list that contain specific substring: ['black']
+# Substring to search: abc
+# Elements of the said list that contain specific substring: []
+
+sample_list = ['red', 'black', 'white', 'green', 'orange']
+substring = 'ack'
+output = list(filter(lambda x: substring in x, sample_list))
+print(list(output))
