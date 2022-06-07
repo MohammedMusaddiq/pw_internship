126b472c9bed1dbffd985ce9403e4a2592117937
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-07 14:02:41 +0530

    lambda ex 5

---
+++ 07_06_2022/lambda_exercise_5.py
@@ -0,0 +1,5 @@
+# 5) Write a Python program to check whether a given string is number or not using Lambda.
+
+is_number = lambda x: print('Number=True') if type(x) == int else print("Number=False")
+
+is_number(1)
