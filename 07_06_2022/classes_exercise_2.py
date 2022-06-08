8b1ebc56f1beacf8656ed062e5f950a252fc97c0
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-08 18:54:22 +0530

    classes ex 2(completed)

--- 07_06_2022/classes_exercise_2.py
+++ 07_06_2022/classes_exercise_2.py
@@ -3,4 +3,23 @@
 # "{{{" are invalid.
 
 
+class MatchParentheses:
+    def __init__(self, string):
+        self.string = string
+        self.parentheses = {'(': ')', '[': ']', '{': '}'}
+        self.container = []
 
+    def is_valid(self):
+        for char in self.string:
+            if char in self.parentheses:
+                self.container.append(char)
+            elif char in self.parentheses.values():
+                if len(self.container) == 0:
+                    return False
+                if self.parentheses[self.container.pop()] != char:
+                    return False
+        return len(self.container) == 0
+
+
+mp = MatchParentheses('(]')
+print(mp.is_valid())
