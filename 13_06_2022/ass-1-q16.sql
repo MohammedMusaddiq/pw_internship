017f871986bae3668aeb22c100c8941152905d5e
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-13 15:28:38 +0530

    ass-1 q-16

---
+++ 13_06_2022/ass-1-q16.sql
@@ -0,0 +1,3 @@
+-- Write a query to fetch minimum salary, maximum salary and average salary from emp table
+
+SELECT MIN(sal) AS minimum_salary, MAX(sal) AS maximum_salary, AVG(sal) AS average_salary FROM emptable
