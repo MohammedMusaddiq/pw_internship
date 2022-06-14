e1498b02ba28e5fabe648173dc2af169f23c9eed
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-14 13:10:33 +0530

    recreated database tables with foreign key

--- 13_06_2022/assignment-1-create-table-insert-data.sql
+++ 13_06_2022/assignment-1-create-table-insert-data.sql
@@ -1,26 +1,35 @@
-CREATE TABLE depttable(deptno integer,dname text,loc text) 
-
-INSERT INTO depttable (deptno,dname,loc)
-VALUES(10 "Accounts" "Bangalore", 20 "IT " "Delhi", 30 "Production" "Chennai",40 "Sales" "Hyd",50 "Admn" "London");
-
-CREATE TABLE EmpTable(
-    EmpNo integer,
-    Ename text,
-    sal integer,
-    Hire_Date date,
-    Commission integer,
-    DeptNo integer,
-    Mgr integer
+CREATE TABLE `department` (
+    `department_no` INT,
+    `department_name` VARCHAR(45),
+    `location` VARCHAR(45),
+    PRIMARY KEY (`department_no`)
 );
 
-INSERT INTO emptable(empno, ename, sal, hire_date, commission, deptno, mgr)
-VALUES (
-    (1001, 'Sachin', '19000, '1-Jan-1980', 2100, 20, 1003),
-    (1002, 'Kapil', 15000, '1-Jan-1970', 2300, 10, 1003),, 
-    (1003, 'Stefen', 12000, '1-Jan-1990', 500, 20, 1007),,
-    (1004, 'Williams', 9000, '1-Jan-2001', NULL, 30, 1007),
-    (1005, 'John', 5000, '1-Jan-2005', NULL, 30, 1006),
-    (1006, 'Dravid', 19000, '1-Jan-1985,' 2400, 10, 1007),
-    (1007, 'Martin', 21000, '1-Jan-2000', 1040, NULL, NULL)
+INSERT INTO `department` VALUES (10,'Accounts','Bangalore');
+INSERT INTO `department` VALUES (20,'IT','Delhi');
+INSERT INTO `department` VALUES (30,'Production','Chennai');
+INSERT INTO `department` VALUES (40,'Sales','Hyd');
+INSERT INTO `department` VALUES (50,'Admin','London');
 
+
+CREATE TABLE `employee` (
+    `emp_no` INT,
+    `emp_name` VARCHAR(45),
+    `salary` INT,
+    `hire_date` DATE,
+    `commission` INT,
+    `department_no` INT,
+    `manager_id` INT,
+    PRIMARY KEY (`emp_no`),
+    FOREIGN KEY (`department_no`) REFERENCES `department`(`department_no`),
+    FOREIGN KEY (`manager_id`) REFERENCES `employee`(`emp_no`)
 );
+INSERT INTO `employee` VALUES (1001,'Sachin',19000,'1990-01-01',2100,20,1003);
+INSERT INTO `employee` VALUES (1002,'Kapil',15000,'1970-01-01',2300,10,1003);
+INSERT INTO `employee` VALUES (1003,'Stefen',12000,'1990-01-01',500,20,1007);
+INSERT INTO `employee` VALUES (1004,'Williams',9000,'2001-01-01',NULL,30,1007);
+INSERT INTO `employee` VALUES (1005,'John',5000, '2005-01-01',NULL,30,1006);
+INSERT INTO `employee` VALUES (1006,'Dravid',19000,'1985-01-01',2400,10,1007);
+INSERT INTO `employee` VALUES (1007,'Martin',21000,'2000-01-01',1040,NULL,NULL);
+
+    
