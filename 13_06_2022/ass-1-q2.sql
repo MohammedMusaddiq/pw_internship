d6dcd08c59026cc527904f6ac4222602d3025bdf
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-14 13:54:28 +0530

    added question

--- 13_06_2022/ass-1-q2.sql
+++ 13_06_2022/ass-1-q2.sql
@@ -1,3 +1,5 @@
+-- Write a query to fetch all the dept details with more than 1 Employee.
+
 SELECT department.department_name, COUNT(*) AS `number of employees`
   FROM department 
      INNER JOIN employee
