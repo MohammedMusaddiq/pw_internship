e58c66495eb996f3522965816fa58541aa2198db
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-15 14:57:07 +0530

    assignment 2 question 08

---
+++ 15_06_2022/assignment-2-q-08.sql
@@ -0,0 +1,12 @@
+-- Fetch the Customer details, who has placed the first and last order
+
+
+-- Fetch the customer details, who has placed multiple orders in the same year
+
+select customer_id, year(order_date), count(*)
+from `order`
+group by customer_id,year(order_date)
+order by count(*) desc
+limit 1
+
+
