dff3fe1ee8b59720296a0e2424498d608ed9608a
Mohammed Musaddiq <musaddiq838@gmail.com>
2022-06-15 11:26:04 +0530

    assignment 2 question 07

---
+++ 15_06_2022/assignment-2-q-07.sql
@@ -0,0 +1,12 @@
+-- Fetch the customer details , who has placed more number of orders
+
+select c.customer_id, c.customer_name, count(*) as order_count
+from customer c,
+     `order` o
+where c.customer_id = o.customer_id
+group by c.customer_id
+order by order_count desc;
+
+
+
+
