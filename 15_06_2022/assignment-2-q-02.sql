7473f160196c03b60bcbced18b42dc5582cdcf67
Mohammed Musaddiq <75469721+MohammedMusaddiq@users.noreply.github.com>
2022-06-15 11:55:14 +0530

    Update assignment-2-q-02.sql

--- 15_06_2022/assignment-2-q-02.sql
+++ 15_06_2022/assignment-2-q-02.sql
@@ -1,9 +1,10 @@
 -- 2. Fetch Order_Id, Ordered_Date, Total Price of the order (product price*qty).
 
-select o.order_id, o.order_date, p.product_price * od.quantity
+select o.order_id, o.order_date, sum(p.product_price * od.quantity) as totla_price
 from `order` o,
      order_details od,
      product p
 where o.order_id = od.order_id
   and od.product_id = p.product_id
+group by o.order_id, o.order_date
 
