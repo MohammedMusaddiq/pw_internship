2543bfd9028aad0e601d1a2e53a7d35e7c7e1bbf
Mohammed Musaddiq <75469721+MohammedMusaddiq@users.noreply.github.com>
2022-06-17 10:53:52 +0530

    updated solution with approach 2

--- 15_06_2022/assignment-2-q-08.sql
+++ 15_06_2022/assignment-2-q-08.sql
@@ -7,3 +7,27 @@ FROM
 GROUP BY customer_id , YEAR(order_date)
 ORDER BY COUNT(*) DESC
 LIMIT 1
+
+-- approach 2
+
+SELECT *
+from (select *,
+             case
+                 when customer_id = lead(customer_id) over (
+                     order by year(order_date)
+                     )
+                     and year(order_date) = lead(year(order_date)) over (
+                         order by year(order_date)
+                         ) then 'yes'
+                 when customer_id = lag(customer_id) over (
+                     order by year(order_date)
+                     )
+                     and year(order_date) = lag(year(order_date)) over (
+                         order by year(order_date)
+                         ) then 'yes'
+                 else 'null'
+                 end flag
+      from `order`
+      order by customer_id,
+               year(order_date)) as x
+where x.flag = 'yes'
