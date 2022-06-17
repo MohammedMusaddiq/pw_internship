4a2fddd5650a451c190e1fc31550a9c0376393a1
Mohammed Musaddiq <75469721+MohammedMusaddiq@users.noreply.github.com>
2022-06-17 08:24:36 +0530

    Update assignment-2-q-08.sql

--- 15_06_2022/assignment-2-q-08.sql
+++ 15_06_2022/assignment-2-q-08.sql
@@ -1,7 +1,7 @@
 -- Fetch the customer details, who has placed multiple orders in the same year
 
 SELECT 
-    customer_id, YEAR(order_date), COUNT(*)
+    customer_id, YEAR(order_date), COUNT(*) as 'No of orders'
 FROM
     `order`
 GROUP BY customer_id , YEAR(order_date)
