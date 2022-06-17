c2f2cd0b7e8c26e21a56a1c49e20a465b22de75c
Mohammed Musaddiq <75469721+MohammedMusaddiq@users.noreply.github.com>
2022-06-17 08:25:24 +0530

    Update assignment-2-q-06.sql

--- 15_06_2022/assignment-2-q-06.sql
+++ 15_06_2022/assignment-2-q-06.sql
@@ -1,14 +1,26 @@
 -- Fetch the Customer details, who has placed the first and last order
 
-SELECT 
-    customer_id, MIN(order_date), MAX(order_date)
-FROM
-    `order`
-GROUP BY customer_id
-ORDER BY MIN(order_date)
-LIMIT 1
 
 
+with date_cte as (
+    select
+        customer_id,
+        order_date,
+        dense_rank() over(
+            order by
+                order_date desc
+        ) as date_ord
+    from
+        `order`
+)
+select
+    customer_id,
+    order_date as 'order dates'
+from
+    date_cte
+where
+    date_ord = 1
+    or date_ord = 10;
 
 
 
